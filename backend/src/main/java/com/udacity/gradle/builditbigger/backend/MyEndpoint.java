23349b0a798da93672a5639195bfeb31f04e39f6
Ewa Śniecińska <ewa.sniecinska@gmail.com>
2018-07-26 23:03:09 +0200

    Deleted unnecessary dependencies

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,7 +3,6 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-import com.google.api.server.spi.config.Named;
 import com.popular.android.java_lib.JokeGenerator;
 
 /** An endpoint class we are exposing */
@@ -24,12 +23,5 @@ public class MyEndpoint {
         response.setData(new JokeGenerator().getJoke());
         return response;
     }
-
-   @ApiMethod(name = "sayHi")
-   public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData(name);
-        return response;
-   }
 
 }
